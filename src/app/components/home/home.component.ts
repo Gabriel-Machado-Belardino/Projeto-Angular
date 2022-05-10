db72bf04b0b36d1df4c407c83a1b6518f55a17d3
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-05-10 20:02:52 -0300

    Aula 17 VARIAVEIS

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+  nomeProduto: string = 'Curso de Angular';
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
+
+  constructor() {
+    //Variaveis de string com concatenação
+    //this.anuncio = 'O ' + this.nomeProduto + 'está em promoção';
+
+    console.log('Nome do Produto: ', this.nomeProduto);
+    console.log('Anuncio: ', this.anuncio);
+    console.log('Id do Produto: ', this.idProduto);
+    console.log('Preço do Produto: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+
+
+    var variavel1;
+    let variavel2;
+    // const variavel3;
+
+    var idade = 10;
+    console.log('Minha idade é ', idade);
+
+    /*
+    function ImprimirIdade()
+    {
+      var idade = 50
+      console.log('Minha idade é ', idade);
+    }
+    ImprimirIdade();
+    */
+
+
+    function ImprimirIdade()
+    {
+      for(let idade = 30; idade <= 40; idade++)
+      {
+        console.log('Minha idade dentro do for é', idade);
+      }
+      console.log('Minha idade fora do for é ', idade);
+
+    }
+    ImprimirIdade();
+
+  }
 
   ngOnInit(): void {
   }
