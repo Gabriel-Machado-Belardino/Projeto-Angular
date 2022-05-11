18ff5c6b1d884af140769c9d695255863f06a789
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-05-10 21:38:34 -0300

    Aula 19 PIPES

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,10 +10,11 @@ export class HomeComponent implements OnInit {
 
   nomeProduto: string = 'Curso de Angular';
   anuncio: string = `O ${this.nomeProduto} está em promoção`;
-  idProduto: number = 123;
-  precoProduto: number = 2.59;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
   promocao: boolean = true;
   foto: string = 'assets/img/crud.png';
+  dataValidade = '2021-12-31'
 
   constructor() {
     //Variaveis de string com concatenação
