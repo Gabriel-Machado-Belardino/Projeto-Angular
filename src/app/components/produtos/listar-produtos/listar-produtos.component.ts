ddb25fc9a933e881580c17add1b18d598ab292c6
Gabriel-Machado-Belardino <gm.belardino@hotmail.com>
2022-05-31 17:41:02 -0300

    Aula 21 Complete

--- src/app/components/produtos/listar-produtos/listar-produtos.component.ts
+++ src/app/components/produtos/listar-produtos/listar-produtos.component.ts
@@ -7,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProdutosComponent implements OnInit {
 
-  constructor() { }
+  listaStrings: string[] = ['Primeiro','Segundo','Terceiro'];
+  listaNumeros: Number[] = [15, 15.18, 100];
+
+  objetoModel = {
+    nome: 'Gabriel',
+    idade: 17,
+    altura: 1.75,
+    graduado: false
+
+  };
+
+  listaProdutos: any[] = [
+    {nome: 'Curso de Angular', precoProduto: 35.56, validade: '2022-06-30', id: 1},
+    {nome: 'Curso de Ionic', precoProduto: 50, validade: '2022-06-30', id: 2, promocao: true},
+    {id: 3, nome: 'Curso de Ionic Avançado', precoProduto: 50, validade: '2022-06-30'}
+    //{nome: '', precoProduto: 0, validade: '0000-00-00', id: 2}
+  ];
+
+  constructor() {
+    
+    for(let item of this.listaStrings)
+    {
+      console.log(item);
+    }
+
+    for(const item of this.listaNumeros)
+    {
+      console.log(item);
+    }
+
+    console.log(this.objetoModel);
+
+   }
 
   ngOnInit(): void {
   }
